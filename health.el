f266a60e11a54f10c92d8beb943589704a5b7981
ekaschalk <ekaschalk@gmail.com>
2017-02-09 23:47:26 -0800

    Dash -let macros utilized, more standardization

--- health.el
+++ health.el
@@ -1,22 +1,30 @@
 ;;; Unused
 (defun health--prompt-table-name ()
   "Collects and prompts a table name"
-  (health--read
+  (health--prompt-read
    "Table"
    (org-element-map (org-element-parse-buffer 'element) 'table
      (-partial #'org-element-property :name))))
 
 ;;; Tables
-;;;; Access
-(defun health--table-headers (TABLE)
-  (nth 0 TABLE))
-(defun health--table-options (TABLE)
-  (nth 1 TABLE))
-(defun health--table-values (TABLE)
+;;;; Parsers
+(defun health--parse-table-headers (TABLE)
+  (car TABLE))
+(defun health--parse-table-options (TABLE)
+  (--map (s-split "/" it t) (nth 1 TABLE)))
+(defun health--parse-table-data (TABLE)
   (-slice TABLE (+ 1 (-elem-index 'hline TABLE))))
 
+(defun health--parse-tables ()
+  "Gets list of plists of table properties"
+  (org-element-map (org-element-parse-buffer 'element) 'table
+    (lambda (table)
+      (--when-let (org-element-property :name table)
+        (-list :name it :contents-begin
+               (org-element-property :contents-begin table))))))
+
 ;;;; Prompts
-(defun health--read (HEADER OPTIONS)
+(defun health--prompt-read (HEADER OPTIONS)
   "HEADER prompts OPTIONS a list of strings or nil for non-helm reading"
   (if (not OPTIONS)
       (read-string (concat HEADER ": "))
@@ -26,43 +34,35 @@
 
 (defun health--prompt-row (TABLE)
   "Prompts a row for TABLE according to its headers and options."
-  (-zip-with 'health--read
-             (health--table-headers TABLE)
-             (--map (s-split "/" it t)
-                   (health--table-options TABLE))))
+  (-zip-with 'health--prompt-read
+             (health--parse-table-headers TABLE)
+             (health--parse-table-options TABLE)))
 
 (defun health--prompt-table ()
   "Prompts tables and returns its lisp representation"
   (save-excursion
-    (let* ((tables
-            (org-element-map (org-element-parse-buffer 'element) 'table
-              (lambda (table)
-                (when-let ((name (org-element-property :name table)))
-                  (-list ':name
-                         name
-                         ':contents-begin
-                         (org-element-property :contents-begin table))))))
-           (selection
-            (health--read "Table" (--map (plist-get it ':name) tables)))
-           (table
-            (--first (string= selection (plist-get it ':name)) tables))
-           (pos
-            (plist-get table :contents-begin)))
+    (-let* ((tables (health--parse-tables))
+            (table-name
+             (health--prompt-read "Table" (--map (plist-get it :name) tables)))
+            (table
+             (--first (string= table-name (plist-get it :name)) tables))
+            ((&plist :contents-begin pos) table))
       (goto-char pos)
       (org-table-to-lisp))))
 
 ;;;; Filters
 (defun health--narrow-table (TABLE)
   "Filters TABLE on prompted column"
-  (let* ((headers (health--table-headers TABLE))
-         (column (-elem-index
-                  (health--read "Filter" headers) headers))
-         (options (-distinct
-                   (-select-column column (health--table-values TABLE)))))
-    (health--read "By value" options)))
+  (let* ((headers (health--parse-table-headers TABLE))
+         (table-data (health--parse-table-data TABLE))
+         (filter (health--prompt-read "Filter" headers))
+         (column (-elem-index filter headers))
+         (options (-distinct (-select-column column table-data))))
+    (health--prompt-read "By value" options)))
 
 ;;;; Utilities
 (defun health--add-to-table ()
   "Prompts and appends row to TABLE"
-  (let ((table (health--prompt-table)))
-    (-snoc table (health--prompt-row table))))
+  (let* ((table (health--prompt-table))
+         (row (health--prompt-row table)))
+    (-snoc table row)))
